2cb6e191131083ab414b888ba71522a7c2195cf3
xiatingyu <xiatingyu1012@163.com>
2024-05-16 13:48:02 +0800

    upload

---
+++ finetune.sh
@@ -0,0 +1,69 @@
+GPUS_PER_NODE=$(python -c 'import torch; print(torch.cuda.device_count())')
+NNODES=1
+NODE_RANK=0
+MASTER_ADDR=localhost
+MASTER_PORT=6006
+ 
+MODEL="../model/Qwen-14B-chat" 
+DATA="data/cnn/train.json"
+output_qwen="model/Qwen-14B-cnn"
+
+function usage() {
+    echo '
+Usage: bash finetune/finetune_ds.sh [-m MODEL_PATH] [-d DATA_PATH]
+'
+}
+
+while [[ "$1" != "" ]]; do
+    case $1 in
+        -m | --model )
+            shift
+            MODEL=$1
+            ;;
+        -d | --data )
+            shift
+            DATA=$1
+            ;;
+        -h | --help )
+            usage
+            exit 0
+            ;;
+        * )
+            echo "Unknown argument ${1}"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+torchrun $DISTRIBUTED_ARGS finetune.py \
+    --model_name_or_path $MODEL \
+    --data_path $DATA \
+    --bf16 True \
+    --output_dir $output_qwen \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 10 \
+    --learning_rate 2e-5 \
+    --weight_decay 0.1 \
+    --adam_beta2 0.95 \
+    --warmup_ratio 0.01 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --report_to "none" \
+    --model_max_length 4096 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True \
+    --deepspeed ds_config_zero3.json
